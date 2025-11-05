ef05d3779eb0f40b8d1fa751f9fe90efeada86cb
viriyadhika <viriya.dhika0@gmail.com>
2025-11-05 11:35:36 -0500

    wandb appi key

--- setup_model.sh
+++ setup_model.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+set -e
 
-sudo apt update -y
-sudo apt install -y docker.io
+# ---------- Install dependencies ----------
+sudo dnf update -y
+sudo dnf install -y docker awscli
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker run -d -p 80:80 viriyadhika/disaster-classification-mscac-model:latest
+
+
+# ---------- Fetch model service IP ----------
+WANDB_API_KEY=$(aws ssm get-parameter \
+  --name "/mlapp/model/WandbApiKey" \
+  --query "Parameter.Value" \
+  --output text 2>/dev/null || echo "")
+
+# ---------- Run Model ----------
+sudo docker run -d --restart always \
+  --name model \
+  -p 80:80 \
+  -e "WANDB_API_KEY=${WANDB_API_KEY}" \
+  viriyadhika/disaster-classification-mscac-model:latest
