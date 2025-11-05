ae2b6d214d9fec7bde2e04dfa716d065e576850d
viriyadhika <viriya.dhika0@gmail.com>
2025-11-04 20:33:50 -0500

    model service url

--- setup_be.sh
+++ setup_be.sh
@@ -6,10 +6,9 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 MODEL_SERVICE_HOST=$(aws ssm get-parameter --name "/mlapp/model/PublicIP" --query "Parameter.Value" --output text 2>/dev/null)
-MODEL_SERVICE_PORT="80"
 
 sudo docker run -d -p 80:80 \
-  -e MONGO_URI="" \
-  -e MODEL_SERVICE_HOST="model-svc" \
-  -e MODEL_SERVICE_PORT="80" \
+  -e "MONGO_URI=" \
+  -e "MODEL_SERVICE_HOST=${MODEL_SERVICE_HOST}" \
+  -e "MODEL_SERVICE_PORT=80" \
   viriyadhika/disaster-classification-mscac:latest
